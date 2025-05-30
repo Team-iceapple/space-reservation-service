73a8099a2e03d86ede49343a9f2aab6dee92fc4c
Boyeon-Shin <anna007096@gmail.com>
2025-05-30 19:42:52 +0900

    initial: 프로젝트 세팅

---
+++ src/main/java/iceapple/spacereservationservice/SpaceReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package iceapple.spacereservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceReservationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iceapple/spacereservationservice/SpaceReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package iceapple.spacereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
